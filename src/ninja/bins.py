4c01c14b153425ce3a72b4c2924af537cd0f8eea
Tim Menzies <tim.menzies@gmail.com>
2016-05-24 23:47:07 -0400

    saving

--- src/ninja/bins.py
+++ src/ninja/bins.py
@@ -1,5 +1,5 @@
 from __future__ import division,print_function
-import sys,os,random,math
+import sys,os,random,math,re
 sys.dont_write_bytecode=True
 
 import cProfile
@@ -13,7 +13,7 @@ class o:
     return str({k:v for k,v
                 in i.__dict__.items()})
  
-The=o(bins=o(attr    = 1,      # a label for the ranges
+The=o(bins=o(attr    = 0,      # a label for the ranges
              cohen   = 0.2,    # 'small' means sd()*cohen
              trivial = 1.05,   # need at least a 5% improvement
              enough  = None,   # enough items for a bin. default=n**0.5
@@ -27,13 +27,24 @@ def xx(z): return z.x
 def yy(z): return z.y
 def same(z): return z
 
+def thing(x):
+  try: return int(x)
+  except ValueError:
+    try: return float(x)
+    except ValueError:
+      return x
+          
 class Col:
   def __init__(i,inits=[],get=same):
     i.reset()
     i.get = get
     map(i.__add__,inits)
-  def __add__(i,x): x = i.get(x); if not isMissing(x): i.add(x)
-  def __sub__(i,x): x = i.get(x); if not isMissing(x): i.sub(x)
+  def __add__(i,x):
+    x = i.get(x)
+    if not isMissing(x): i.add(x)
+  def __sub__(i,x):
+    x = i.get(x)
+    if not isMissing(x): i.sub(x)
   def __repr__(i):
     return str(kv(i))
   
@@ -42,11 +53,11 @@ class Sym(Col):
     i.counts, i.most, i.mode, i.n = {},0,None,0
   def add(i,x):
     i.n += 1
-    new = i,counts[x] = i.counts.get(x,0) + 1
+    new = i.counts[x] = i.counts.get(x,0) + 1
     if new > i.most:
       i.most, i.mode = new,x
   def sub(i,x):
-    i.n += 1
+    i.n -= 1
     i.counts[x] -= 1
     if x == i.mode:
       i.most, i.mode = None,None
@@ -55,7 +66,7 @@ class Sym(Col):
   def ent(i):
     tmp = 0
     for val in i.counts.values():
-      p /= i.n
+      p = val/i.n
       if p:
         tmp -= p*math.log(p,2)
     return tmp
@@ -101,8 +112,7 @@ def sdiv(lst,
          xx      = same,   # access independent variable
          yy      = same):  # access dependent   variable
   # ---------------------------------------------------
-  def divide(lst, out=[], lvl=0):
-    arg          = None
+  def divide(lst, out=[], lvl=0, arg=None):
     xlhs, xrhs   = Num(get=xx), Num(lst, get=xx)
     ylhs, yrhs   = Num(get=yy), Num(lst, get=yy)
     score,score1 = yrhs.sd(),None
@@ -138,35 +148,59 @@ def sdiv(lst,
   enough = enough or len(lst)**0.5
   return divide( sorted(lst[:], key=xx), out=[] ,lvl=0) # copied, sorted
 
-def ediv(lst, tiny=2,
-         xx=lambda x:x[0], uu=lambda x:x[1]):
- #XXX
-  #----------------------------------------------
-  def divide(lst, out=[], lvl=0):
+def ediv(lst,
+         attr    = The.bins.attr,
+         cohen   = The.bins.cohen,
+         trivial = The.bins.trivial,
+         enough  = The.bins.enough,
+         small   = The.bins.small,
+         verbose = The.bins.verbose,
+         xx      = lambda z:z[0],
+         yy      = lambda z:z[-1]):
+  def divide(lst, out=[], lvl=0, arg=None):
     def ke(z): return z.k()*z.ent()
-    lhs,rhs   = Counts(),Counts(sym(x) for x in this)
-    n0,k0,e0,ke0= 1.0*rhs.n,rhs.k(),rhs.ent(),ke(rhs)
-    cut, least  = None, e0
-    for j,x  in enumerate(this):
-      #print lhs.n,rhs.n,tiny,"as"
-      if lhs.n > tiny and rhs.n > tiny:
-        maybe= lhs.n/n0*lhs.ent()+ rhs.n/n0*rhs.ent()
-        if maybe < least :
-          cut,least = j,maybe
-      rhs - sym(x)
-      lhs + sym(x)
-    #print cut,e,"asd",len(cuts)
-    if cut:
+    arg            = None
+    xlhs, xrhs     = Num(get=xx), Num(lst, get=xx)
+    ylhs, yrhs     = Sym(get=yy), Sym(lst, get=yy)
+    k0, e0, ke0    = yrhs.k(), yrhs.ent(), ke(yrhs)
+    score,score1   = yrhs.ent(),None
+    n = len(lst)
+    for i,new  in enumerate(lst):
+      xlhs + new; xrhs - new
+      ylhs + new; yrhs - new
+      if xrhs.n < enough:
+        break
+      else:
+        if xlhs.n >= enough:
+          start, here, stop = xx(lst[0]), xx(new), xx(lst[-1])
+          if here - start > small:
+            if stop - here > small:
+              score1= ylhs.n/n*ylhs.ent()+ yrhs.n/n*yrhs.ent()
+              if score1*trivial < score:
+                gain   = e0 - score1
+                delta  = math.log(3**k0-2,2)-(ke0- ke(yrhs)-ke(ylhs))
+                border = (math.log(n-1,2) + delta)/n
+                if gain >= border:
+                  arg,score = i,score1
+  #----------------------------------------------
+    if verbose:
+      print('.. '*lvl,len(lst),score1 or '.')
+    if arg:
       divide(lst[:arg], out=out, lvl=lvl+1)
-      dviide(lst[arg:], out=out, lvl=lvl+1)
+      divide(lst[arg:], out=out, lvl=lvl+1)
     else:
-      out += [(e,this)]
+      out.append(Range(attr=attr, score=score,
+                       n=len(lst), id=len(out),
+                       lo=xx(lst[0]), up=xx(lst[-1]),
+                       has=lst))                       
     return out
   #---| main |-----------------------------------
   if not lst: return []
-  return divide(sorted(lst,key=xx),out=[],lvl=0)
+  small  = small  or Num(lst,get=xx).small(cohen)
+  enough = enough or len(lst)**0.5
+  return divide(sorted(lst,key=xx), out=[], lvl=0)
   
-def _bins():
+def _sdiv():
   rseed(1)
   n   = 1000
   lst = [r()**2 for x in xrange(n)]
@@ -174,9 +208,16 @@ def _bins():
   for y in sdiv(lst):
     print(y)
  
-_bins()
-#cProfile.run('_bins()')    
-sys.exit()
+def _ediv():
+  rseed(1)
+  lst1 = list("abcd")
+  lst2 = []
+  for _ in xrange(1000):
+    for i,x in enumerate(lst1):
+      lst2 += [[i+0.5*r(), x]]
+  lst2 = sorted(lst2)
+  for y in ediv(lst2):
+    print(y)
          
 class row:
   def __init__(i,x=None,y=None):
@@ -192,17 +233,49 @@ class tub:
     lst = i.get(lst)
     for j,val in enumerate(lst):
       if not isMissing(val):
-        about = i.about.get(j,None)
+        about = i.abouts.get(j,None)
         if not about:
-          about = i.about[j] = sym() if isSym(val) else num()
+          about = i.abouts[j] = Sym() if isSym(val) else Num()
         about + val
         
 class twintub:
-  def __init__(i):
+  def __init__(i,xx=xx,yy=yy):
     i.x=tub(xx)
     i.y=tub(yy)
+    i._rows = []
   def __add__(i,row):
     i.x + row
     i.y + row
+    i._rows += [row]
+
+class arff:
+  def __init__(i, data):
+    i.tubs    = twintub(xx=lambda z: z[:-1],
+                       yy=lambda z: z[-1])
+    i.bad    = r'(["\'\r\n]|#.*)'
+    i._header = []
+    i.read(data)
+  def atData(i,x):
+    return re.match(r'^[ \t]*@DATA',
+                    x,re.IGNORECASE)
+  def header(i):
+    return "\n".join(i._header)
+  def read(i,data):
+    pre    = True
+    with open(data)  as f:
+      for line in f:
+        line = re.sub(i.bad, "", line)
+        if pre:
+          i._header += [line]
+        if i.atData(line):
+          pre    = False
+        elif not pre:
+          i.tubs + map(thing, line.split(","))
   
-    
+if __name__ == "__main__":
+  #_sdiv()
+  #print("")
+  #_ediv()
+  a=arff("data/jedit-4.1.arff")
+  print(a.header())
+
